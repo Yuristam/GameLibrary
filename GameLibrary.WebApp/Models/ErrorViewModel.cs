d1c2245199e6503db541487864dd3bf45714c2a6
Yuristam <yuristamasanakunov@gmail.com>
2023-01-07 21:39:59 +0600

    Add project files.

---
+++ GameLibrary.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameLibrary.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
